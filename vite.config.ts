6aaa37ca2f524e57b74725c0ebd524b9f0317e87
xxcodianxx <44289933+xxcodianxx@users.noreply.github.com>
2023-10-18 23:56:12 +1030

    refactor: stabilize css classes based on component name

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,21 @@ import { ConfigEnv, defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import Icons from "unplugin-icons/vite";
 
-import { execSync } from "child_process"; 
+import { execSync } from "child_process";
+
+function zvelteCssHash({ name, filename, css, hash }): string {
+    // eg. z-ServerList, z-Tooltip, z-Button
+    return `z-${name}`;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig((env: ConfigEnv) => {
     let version;
     try {
-        version = execSync('git rev-parse HEAD').toString().trimEnd().substr(0, 7);
+        version = execSync("git rev-parse HEAD")
+            .toString()
+            .trimEnd()
+            .substr(0, 7);
     } catch (e) {
         console.error("Failed to get commit hash");
         version = "unknown";
@@ -16,13 +24,13 @@ export default defineConfig((env: ConfigEnv) => {
 
     return {
         plugins: [
-            svelte(),
+            svelte({ compilerOptions: { cssHash: zvelteCssHash } }),
             Icons({
                 compiler: "svelte",
             }),
         ],
         define: {
             ZLING_VERSION: JSON.stringify(version),
-        }
+        },
     };
 });
