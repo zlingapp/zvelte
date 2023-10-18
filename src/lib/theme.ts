75585266cf1ebef7ec1a5d7ed5d0ad8f1a2ed568
xxcodianxx <44289933+xxcodianxx@users.noreply.github.com>
2023-10-19 00:48:23 +1030

    refactor: add more keys to the default theme string

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -3,13 +3,17 @@ export type Theme = {
     style: string;
 };
 
-export const defaultTheme = `@import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@400;500;700&display=swap');
+export const defaultTheme = `/* Feel free to edit this file to your liking. */
+
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@400;500;700&display=swap');
 
 :root {
+  --accent-color: #2764ff;
   --text-color: rgba(255, 255, 255, 0.87);
   --bg-0: #232323;
   --bg-1: #1C1C1C;
   --bg-2: #0E0E0E;
+  --message-caret-bg: #2b2b2b;
 
   font-family: 'Source Sans 3', sans-serif;
   line-height: 1.5;
